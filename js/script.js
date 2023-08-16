848458f652497ccd4bd63a85ba76510c529284c9
DmytroScimusGit <d.tatochenko@gmail.com>
2023-08-16 13:10:33 +0300

    update deadline

--- js/script.js
+++ js/script.js
@@ -1,47 +1,56 @@
-const deadline = '2021-08-8';
+const deadline = "2023-12-8";
 
-function getTimeRemaining(endtime){
-    
-    let now = new Date();
-    const t = Math.floor((Date.parse(deadline)-Date.parse(now))/(1000*60*60*24)),
-          days = Math.floor((Date.parse(deadline)-Date.parse(now))/(1000*60*60*24)),
-          hours = Math.floor((Date.parse(deadline)-Date.parse(now))/(1000*60*60)%24),
-          minutes = Math.floor((Date.parse(deadline)-Date.parse(now))/(1000*60)%60),
-          seconds =Math.floor((Date.parse(deadline)-Date.parse(now))/(1000)%60);
-    return {
-        total:t,
-        days:days,
-        hours:hours,
-        minutes:minutes,
-        seconds:seconds
-    }
+function getTimeRemaining(endtime) {
+  let now = new Date();
+  const t = Math.floor(
+      (Date.parse(deadline) - Date.parse(now)) / (1000 * 60 * 60 * 24)
+    ),
+    days = Math.floor(
+      (Date.parse(deadline) - Date.parse(now)) / (1000 * 60 * 60 * 24)
+    ),
+    hours = Math.floor(
+      ((Date.parse(deadline) - Date.parse(now)) / (1000 * 60 * 60)) % 24
+    ),
+    minutes = Math.floor(
+      ((Date.parse(deadline) - Date.parse(now)) / (1000 * 60)) % 60
+    ),
+    seconds = Math.floor(
+      ((Date.parse(deadline) - Date.parse(now)) / 1000) % 60
+    );
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
-function setTimer(selector,endtime){
-    let timer = document.querySelector(selector),
-        days = document.querySelector('#days'),
-        hours = document.querySelector('#hours'),
-        minutes = document.querySelector('#minutes'),
-        seconds = document.querySelector('#seconds'),
-        interval = setInterval(updateClock,1000);
-        updateClock();
-    function plusZero(num){
-        if(num >= 0 && num < 10){
-            return num.innerHTML = `0${num}`;
-        }else{
-            return num;
-        }
+function setTimer(selector, endtime) {
+  let timer = document.querySelector(selector),
+    days = document.querySelector("#days"),
+    hours = document.querySelector("#hours"),
+    minutes = document.querySelector("#minutes"),
+    seconds = document.querySelector("#seconds"),
+    interval = setInterval(updateClock, 1000);
+  updateClock();
+  function plusZero(num) {
+    if (num >= 0 && num < 10) {
+      return (num.innerHTML = `0${num}`);
+    } else {
+      return num;
     }
-    
-    function updateClock(){
-        const t = getTimeRemaining(endtime);
-            days.innerHTML = plusZero(t.days);
-            hours.innerHTML = plusZero(t.hours);
-            minutes.innerHTML = plusZero(t.minutes);
-            seconds.innerHTML = plusZero(t.seconds);
-            if(t.total<= 0){
-                clearInterval(interval);
-            }
+  }
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+    days.innerHTML = plusZero(t.days);
+    hours.innerHTML = plusZero(t.hours);
+    minutes.innerHTML = plusZero(t.minutes);
+    seconds.innerHTML = plusZero(t.seconds);
+    if (t.total <= 0) {
+      clearInterval(interval);
     }
+  }
 }
 
-setTimer('#timer',deadline);
+setTimer("#timer", deadline);
